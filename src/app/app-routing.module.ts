613df4147098898ef4aa07e1883ef9ebf8fce44a
Xamarin Dev <senya5666@mail.ru>
2020-01-22 15:04:27 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutusComponent } from './components/shared/aboutus/aboutus.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', redirectTo:'/aboutus', pathMatch:'full'},
+{path:'aboutus', component:AboutusComponent},
+{path:'header', component: HeaderComponent},
+{path:'footer', component:FooterComponent},
+{path:'**', redirectTo:'/aboutus', pathMatch:'full'},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { BooksListComponent } from './components/books-list/books-list.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { BookDetailsComponent } from './components/book-details/book-details.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { AboutusComponent } from './components/shared/aboutus/aboutus.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { CustomMaterialModule } from './custom-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BooksListComponent,
+    RegisterComponent,
+    ShoppingCartComponent,
+    BookDetailsComponent,
+    AdminComponent,
+    HeaderComponent,
+    AboutusComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
